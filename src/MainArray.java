c3f2983c76d2b33be09787f3a4ccf217cb06ab19
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-05-13 20:16:42 +0300

    -HW first edition, 13/05

--- src/MainArray.java
+++ src/MainArray.java
@@ -4,7 +4,7 @@ public class MainArray {
     public static void main(String[] args) {
         ArrayStorage storage = new ArrayStorage();
 
-        for (int i = 0; i < storage.sizeOfStorage()- 9990; i++) {
+        for (int i = 0; i < storage.sizeOfStorage() - 9990; i++) {
             storage.saveResume(new Resume(i, "kolya", 41));
         }
         System.out.println(storage.sizeOfStorage());
--- src/Resume.java
+++ src/Resume.java
@@ -2,13 +2,14 @@ public class Resume {
     private int uuid;
     private String name;
     private int age;
-    public Resume (int uuid, String name, int age){
+
+    public Resume(int uuid, String name, int age) {
         this.uuid = uuid;
         this.name = name;
         this.age = age;
     }
-    public String toString()
-    {
+
+    public String toString() {
         return uuid + " " + name + " " + age;
     }
 }
