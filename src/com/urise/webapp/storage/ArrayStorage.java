51760489d660e7126cb19f3a19ca70425059f72b
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-05-26 20:05:40 +0300

    - HW Lesson 1, 26\05

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -3,37 +3,52 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int counterOfResumes = 0;
+    private int currentIndex;
 
     void clear() {
         Arrays.fill(storage, 0, counterOfResumes, null);
+        counterOfResumes = 0;
     }
 
-    /*Scanner scan = new Scanner(System.in);
-    void update(Resume r) {
-        if (hasResume(r.toString())) {
-            System.out.println("Enter new Resume");
-            r.setUuid(scan.nextLine());
-            System.out.println("Update has been completed");
-        }
-    }*/
+    Scanner scan = new Scanner(System.in);
 
-    boolean hasResume(String uuid) {
-        boolean hasResume = false;
+    void update(Resume r) {
+        String newUuid;
+        if (isExistingResume(r.toString())) {
+            do {
+                System.out.println("Enter new Resume");
+                newUuid = scan.nextLine();
+                if (isExistingResume(newUuid)) {
+                    System.out.println("Duplicate resume, update has been rejected");
+                    break;
+                } else r.setUuid(newUuid);
+                System.out.println("Update has been completed");
+            } while (!isExistingResume(r.toString()));
+        }
+        if (!isExistingResume(r.toString())) {
+            System.out.println("ERROR: " + r.toString() + " doesn't exist");
+        }
+    }
+
+    boolean isExistingResume(String uuid) {
+        boolean isExistingResume = false;
         for (int i = 0; i < counterOfResumes; i++) {
             if (uuid.equals(storage[i].toString())) {
-                hasResume = true;
+                isExistingResume = true;
+                currentIndex = i;
                 break;
             }
         }
-        return hasResume;
+        return isExistingResume;
     }
 
     void save(Resume r) {
-        if (hasResume(r.toString())) {
+        if (isExistingResume(r.toString())) {
             System.out.println("ERROR: " + r.getUuid() + " already exists");
         } else if (counterOfResumes < storage.length) {
             storage[counterOfResumes] = r;
@@ -42,26 +57,17 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        if (hasResume(uuid)) {
-            for (int i = 0; i < counterOfResumes; i++) {
-                if (uuid.equals(storage[i].toString())) {
-                    return storage[i];
-                }
-            }
-        } else System.out.println("ERROR: " + uuid.toString() + " doesn't exist");
+        if (isExistingResume(uuid)) {
+            return storage[currentIndex];
+        } else System.out.println("ERROR: " + uuid + " doesn't exist");
         return null;
     }
 
     void delete(String uuid) {
-        if (hasResume(uuid)) {
-            for (int i = 0; i < counterOfResumes; i++) {
-                if (uuid.equals(storage[i].toString())) {
-                    System.arraycopy(storage, i + 1, storage, i, counterOfResumes - 1);
-                    counterOfResumes--;
-                }
-                break;
-            }
-        } else System.out.println("ERROR: " + uuid.toString() + " doesn't exist");
+        if (isExistingResume(uuid)) {
+            System.arraycopy(storage, currentIndex + 1, storage, currentIndex, counterOfResumes - 1);
+            counterOfResumes--;
+        } else System.out.println("ERROR: " + uuid + " doesn't exist");
     }
 
     Resume[] getAll() {
--- src/com/urise/webapp/storage/MainArray.java
+++ src/com/urise/webapp/storage/MainArray.java
@@ -17,7 +17,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit | update): ");
+            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | update | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -51,10 +51,10 @@ public class MainArray {
                     ARRAY_STORAGE.clear();
                     printAll();
                     break;
-                /*case "update":
-                    r = new Resume();
-                    ARRAY_STORAGE.update(r);
-                    printAll();*/
+                case "update":
+                    ARRAY_STORAGE.update(ARRAY_STORAGE.get(uuid));
+                    printAll();
+                    break;
                 case "exit":
                     return;
                 default:
