5312b5c1f24bd0a7c0ec60a877abdb6b571298b5
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-06-10 07:02:27 +0300

    - HW Lesson 3, corrections

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -8,7 +8,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void save(Resume r) {
-        if (getIndex(r.getUuid()) != -1) {
+        if (getIndex(r.getUuid()) >= 0) {
             System.out.println("Resume " + r.getUuid() + " already exist");
         } else if (size == STORAGE_LIMIT) {
             System.out.println("Storage is overflowed");
