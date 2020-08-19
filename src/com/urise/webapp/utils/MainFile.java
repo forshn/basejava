16b8925f6d937ac417eebe9396f96b2aa2a05d06
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-08-19 21:25:27 +0300

    - Serializable HW + recursion added

--- src/com/urise/webapp/utils/MainFile.java
+++ src/com/urise/webapp/utils/MainFile.java
@@ -5,19 +5,19 @@ import java.io.File;
 public class MainFile {
     public static void main(String[] args) {
         File directory = new File(".\\src\\com\\urise\\webapp");
-        printDirectoryDeeply(directory);
+        printDirectoryDeeply(directory, "");
     }
 
-    private static void printDirectoryDeeply(File directory) {
+    private static void printDirectoryDeeply(File directory, String space) {
         File[] files = directory.listFiles();
 
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(space + "File: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    printDirectoryDeeply(file);
+                    System.out.println(space + "Directory: " + file.getName());
+                    printDirectoryDeeply(file,space + "\t");
                 }
             }
         }
