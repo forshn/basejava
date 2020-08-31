8623b681aa6f5b066834aa82b507c1b88e9e8f4f
Nikolay Forsh <forsh_nikolay@mail.ru>
2020-08-31 21:48:14 +0300

    - corrections

--- src/com/urise/webapp/storage/FileStorage.java
+++ src/com/urise/webapp/storage/FileStorage.java
@@ -1,7 +1,7 @@
 package com.urise.webapp.storage;
 
 import com.urise.webapp.exception.StorageException;
-import com.urise.webapp.model.Resume;
+import com.urise.webapp.*;
 import com.urise.webapp.storage.seralizer.StreamSerializer;
 
 import java.io.*;
