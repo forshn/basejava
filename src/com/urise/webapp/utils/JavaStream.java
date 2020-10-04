4b13c06ca292a427a8027df2dce373788634d845
Nikolay <forsh_nikolay@mail.ru>
2020-10-04 16:58:33 +0300

    - HW 12, JavaStream class added and corrected-2

--- src/com/urise/webapp/utils/JavaStream.java
+++ src/com/urise/webapp/utils/JavaStream.java
@@ -1,14 +1,12 @@
 package com.urise.webapp.utils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class JavaStream {
 
     public static void main(String[] args) {
-        int[] array = new int[]{9, 8, 8, 7, 5, 4, 3, 3, 2, 1, 0};
+        int[] array = new int[]{9, 5, 1, 0};
         System.out.println(minValue(array));
 
         ArrayList<Integer> integers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 9, 1, 2, 4, 5));
@@ -16,16 +14,10 @@ public class JavaStream {
     }
 
     public static int minValue(int[] values) {
-        int result = 0;
-        int num = (Arrays.stream(values)
+        return Arrays.stream(values)
                 .distinct()
-                .reduce(0, (a, b) -> 10 * a + b));
-        while (num != 0) {
-            int digit = num % 10;
-            result = result * 10 + digit;
-            num /= 10;
-        }
-        return result;
+                .sorted()
+                .reduce((a, b) -> 10 * a + b).orElse(-1);
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
